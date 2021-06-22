2e816e87ce3df633d8ac615092b3a0a2d2f4971a
baripembo <erikawei@gmail.com>
2021-06-22 17:36:02 -0400

    updating styling for about page

--- store/index.js
+++ store/index.js
@@ -1,70 +1,10 @@
-import Vue from 'vue'
-
 export const state = () => ({
-  contributions: [],
-  planRequirements: 0,
-  planFunded: 0,
-  codelists: {
-    countries: {},
-    organisationTypes: {},
-    sectors: {}
-  },
-  activityUsedCodelists: {
-    countriesRegions: [],
-    sectors: [],
-    orgNameIndex: [],
-    reportingOrgNames: {},
-    reportingOrgTypes: {}
-  },
-  originalActivityData: [],
-  originalActivityTransactionData: [],
-  originalFlows: [],
-  m49Codelists: {
-    leastDeveloped: [],
-    landLockedDeveloping: [],
-    smallIslandDeveloping: [],
-    developing: [],
-    developed: []
-  },
   tooltips: {},
   orgNameIndex: [],
   isProd: true
 })
 
 export const mutations = {
-  setContributions (state, contributions) {
-    state.contributions = contributions
-  },
-  setPlanRequirements (state, value) {
-    state.planRequirements = value
-  },
-  setPlanFunded (state, value) {
-    state.planFunded = value
-  },
-  setCodelistsCountry (state, codelists) {
-    Vue.set(state.codelists, 'countries', codelists)
-  },
-  setCodelistsSector (state, codelists) {
-    Vue.set(state.codelists, 'sectors', codelists)
-  },
-  setCodelistsOrganisationType (state, codelists) {
-    Vue.set(state.codelists, 'organisationTypes', codelists)
-  },
-  setM49Codelists (state, codelists) {
-    state.m49Codelists = codelists
-  },
-  setActivityUsedCodelists (state, codelists) {
-    state.activityUsedCodelists = codelists
-  },
-  setOriginalActivityData (state, activities) {
-    state.originalActivityData = activities
-  },
-  setOriginalActivityTransactionData (state, activityTransactions) {
-    state.originalActivityTransactionData = activityTransactions
-  },
-  setOriginalFlows (state, flows) {
-    state.originalFlows = flows
-  },
   setTooltips (state, tooltips) {
     state.tooltips = tooltips.reduce((summary, item) => {
       summary[item.key] = item.tooltip
