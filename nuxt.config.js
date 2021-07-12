3927eb13ff1e28c3481d407ea93e296e8c21a28e
baripembo <erikawei@gmail.com>
2021-07-12 16:53:01 -0700

    subfolder path fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 const routerBase = process.env.DEPLOY_ENV === 'WITH_SUBFOLDER'
   ? {
       router: {
-        base: '/visualization/iati-c19-explorer/'
+        base: '/viz-iati-c19-explorer/'
       }
     }
   : {}
