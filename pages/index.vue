8252b3e11e3846229347c46d4f91a41c9986e2ad
baripembo <erikawei@gmail.com>
2021-07-12 16:49:49 -0700

    subfolder paths

--- pages/index.vue
+++ pages/index.vue
@@ -583,7 +583,7 @@ export default {
   methods: {
     async loadData () {
       const dataPath = (this.isProd) ? 'https://ocha-dap.github.io/hdx-scraper-iati-viz/transactions.json' : 'https://mcarans.github.io/hdx-scraper-iati-viz/transactions.json'
-      const filePath = '/'// (config.dev) ? '' : '/viz-iati-c19-explorer/'
+      const filePath = (config.dev) ? '' : '/visualization/iati-c19-explorer/'
       await axios.get(filePath + 'tooltips.csv')
         .then((response) => {
           return csvtojson().fromString(response.data).then((jsonData) => {
