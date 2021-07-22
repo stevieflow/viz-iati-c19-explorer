4e015b9f864c1fb1ceff2ae36ce76182b2997d7c
baripembo <erikawei@gmail.com>
2021-07-22 09:51:17 -0400

    renaming explorer to dashboard

--- pages/about.vue
+++ pages/about.vue
@@ -240,10 +240,10 @@
                   Prepared data (automatically updated every 24 hours)
                   <ul>
                     <li>Activities and transactions — <a href="https://ocha-dap.github.io/hdx-scraper-iati-viz/transactions.csv">transactions.csv</a> | <a href="https://ocha-dap.github.io/hdx-scraper-iati-viz/transactions.json">transactions.json</a></li>
-                    <li>Financial flows — <a href="https://ocha-dap.github.io/hdx-scraper-iati-viz/flows.csv" target="_blank">flows.csv</a> | <a href="https://ocha-dap.github.io/hdx-scraper-iati-viz/flows.json">flows.json</a></li>
+                    <li>Financial flows — <a href="https://ocha-dap.github.io/hdx-scraper-iati-viz/flows.csv" target="_blank">flows.csv</a> | <a href="https://ocha-dap.github.io/hdx-scraper-iati-viz/flows.json" target="_blank">flows.json</a></li>
                   </ul>
                 </li>
-                <li><a href="https://github.com/OCHA-DAP/viz-iati-c19-explorer">Data visualization front end</a></li>
+                <li><a href="https://github.com/OCHA-DAP/viz-iati-c19-dashboard target="_blank"">Data visualization front end</a></li>
               </ul>
             </p>
           </section>
